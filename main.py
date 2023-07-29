4ff383e9021950999f630a5d44fa749cb80b1d8e
Quentin Duverge <quentin.duverge@essencemediacom.com>
2023-07-29 09:05:36 +0100

    none

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
 print('coucou')
+
+# Create a service account and initialize the Firestore client.
+cred = credentials.Certificate("firestore_pulls.json")
+firebase_admin.initialize_app(cred)
+
+# Create a reference to the collection you want to read data from.
+db = firestore.client()
+collection = db.collection("categories")
+
+# Get all the documents in the collection.
+docs = collection.get()
+
+# Iterate over the documents and print their data.
+for doc in docs:
+    print(doc.to_dict())
