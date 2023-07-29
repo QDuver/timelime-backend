9d6dd0e0e2617863a1d4aac780f59d27a59525c5
Quentin Duverge <quentin.duverge@essenceglobal.com>
2023-07-29 11:43:26 +0100

    none

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+from google.cloud import secretmanager
+import json
 
-print('coucou')
+def get_secret(project_id, secret_id):
+    client = secretmanager.SecretManagerServiceClient()
+    secret_name = f"projects/{project_id}/secrets/{secret_id}/versions/latest"
 
-# Create a service account and initialize the Firestore client.
-cred = credentials.Certificate("firestore_pulls.json")
+    try:
+        response = client.access_secret_version(name=secret_name, )
+        return json.loads(response.payload.data.decode("UTF-8"))
+    except Exception as e:
+        print(f"Error retrieving secret: {e}")
+        return None
+
+# Replace 'your_project_id' and 'your_secret_id' with your actual GCP project ID and secret ID.
+project_id = "82528465111"
+secret_id = "firestore_pulls"
+
+
+secret = get_secret(project_id, secret_id)
+cred = credentials.Certificate(secret)
+
 firebase_admin.initialize_app(cred)
 
-# Create a reference to the collection you want to read data from.
 db = firestore.client()
-collection = db.collection("categories")
+collection = db.collection("timelines")
 
-# Get all the documents in the collection.
 docs = collection.get()
 
-# Iterate over the documents and print their data.
 for doc in docs:
     print(doc.to_dict())
