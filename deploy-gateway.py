3d351525bd58a1d95ab430abe5b4fc1eb4a6cc63
Quentin Duverge <quentin.duverge@essenceglobal.com>
2023-08-04 18:42:32 +0100

    none

--- deploy-gateway.py
+++ deploy-gateway.py
@@ -3,4 +3,5 @@ import subprocess
 # output = subprocess.check_output("gcloud endpoints configs list --service=gateway-uavkhjofda-nw.a.run.app", shell = True,  universal_newlines=True)
 # CONFIG_ID = output.splitlines()[1].split(' ')[0]
 CONFIG_ID = "2021-10-14r0"
-print("::set-output name=id::{CONFIG_ID}")
+with open("config-id.txt", "w") as f:
+        f.write(str(CONFIG_ID))
