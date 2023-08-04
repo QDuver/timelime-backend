d5efcca7d0cb804f2db056ad771051d2ad4958d8
Quentin Duverge <quentin.duverge@essenceglobal.com>
2023-08-04 18:23:26 +0100

    none

--- deploy-gateway.py
+++ deploy-gateway.py
@@ -1,5 +1,6 @@
 import subprocess
 
-output = subprocess.check_output("gcloud endpoints configs list --service=gateway-uavkhjofda-nw.a.run.app", shell = True,  universal_newlines=True)
-CONFIG_ID = output.splitlines()[1].split(' ')[0]
-print(f"::set-output name=id::{CONFIG_ID}")
+# output = subprocess.check_output("gcloud endpoints configs list --service=gateway-uavkhjofda-nw.a.run.app", shell = True,  universal_newlines=True)
+# CONFIG_ID = output.splitlines()[1].split(' ')[0]
+CONFIG_ID = "2021-10-14r0"
+print("::set-output name=id::{CONFIG_ID}")
