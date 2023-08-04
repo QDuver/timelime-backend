975f756b564e7a5329313f39b0b90882867fb6f4
Quentin Duverge <quentin.duverge@essenceglobal.com>
2023-08-04 18:03:40 +0100

    none

--- deploy-gateway.py
+++ deploy-gateway.py
@@ -1,6 +1,5 @@
 import subprocess
 
-command = ["gcloud", "endpoints", "configs", "list", "--service=gateway-uavkhjofda-nw.a.run.app"]
-output = subprocess.check_output(command, shell = True,  universal_newlines=True)
+output = subprocess.check_output("gcloud endpoints configs list --service=gateway-uavkhjofda-nw.a.run.app", shell = True,  universal_newlines=True)
 CONFIG_ID = output.splitlines()[1].split(' ')[0]
 print(CONFIG_ID)
