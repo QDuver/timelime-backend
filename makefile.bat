d3fc171423c3854cd43f595d9961e5e5086794c1
Quentin Duverge <quentin.duverge@essencemediacom.com>
2023-08-05 12:23:27 +0100

    None

--- makefile.bat
+++ makefile.bat
@@ -1,22 +1,13 @@
 
 docker build . -t timelime-backend-img
 docker run -p 5000:5000 -e GOOGLE_APPLICATION_CREDENTIALS=secrets/timelime-dev-sa.json timelime-backend-img
-gcloud run deploy timelime-backend --region=europe-west2 --source . --port 5000
+gcloud run deploy timelime-backend --region=europe-west2 --source . --port 5000 --no-allow-unauthenticated
 
 gcloud endpoints services deploy openapi-run.yaml --project timelime-dev
 
-//deploy ESPVv2 default Docker Image
-gcloud run deploy gateway --image="gcr.io/endpoints-release/endpoints-runtime-serverless:2" --allow-unauthenticated --region europe-west2 --platform managed
-
-//Deploy ESPv2 Docker image
-chmod +x gcloud_build_image.sh
-./gcloud_build_image.sh -s gateway-uavkhjofda-nw.a.run.app -c 2023-08-04r0 -p timelime-dev
-gcloud run deploy gateway --image="gcr.io/timelime-dev/endpoints-runtime-serverless:2.45.0-gateway-uavkhjofda-nw.a.run.app-2023-08-04r0" --allow-unauthenticated --region europe-west2 --platform managed --project=timelime-dev
 
 
-https://gateway-uavkhjofda-nw.a.run.app
-curl --request GET --header "content-type:application/json" "https://gateway-uavkhjofda-nw.a.run.app/quentino"
-
-gcloud endpoints configs list --service=gateway-uavkhjofda-nw.a.run.app
-
-
+curl -X POST \
+     -H "Authorization: Bearer $(gcloud auth application-default print-access-token)" \
+     -H "Content-Type: application/json" \
+     -d @request_payload.json https://<CLOUD_RUN_SERVICE_URL>
