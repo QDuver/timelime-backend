c9b55ee0d4a5c1b97b36385313cf5d229bc4185e
Quentin Duverge <quentin.duverge@essencemediacom.com>
2023-08-08 06:55:37 +0100

    none

--- makefile.bat
+++ makefile.bat
@@ -4,13 +4,3 @@ docker run -p 5000:5000 -e GOOGLE_APPLICATION_CREDENTIALS=secrets/timelime-dev-s
 gcloud run deploy timelime-backend --region=europe-west2 --source . --port 5000 --no-allow-unauthenticated
 
 gcloud endpoints services deploy openapi-run.yaml --project timelime-dev
-
-
-
-curl -X POST \
-     -H "Authorization: Bearer $(gcloud auth application-default print-access-token)" \
-     -H "Content-Type: application/json" \
-     -d @request_payload.json https://<CLOUD_RUN_SERVICE_URL>
-
-
-curl -X POST "https://oauth2.googleapis.com/token" -d "grant_type=urn:ietf:params:oauth:grant-type:jwt-bearer&assertion=$(cat secrets/timelime-dev-7f677154d05e.json)"
