b377e7fb3cc69d108af37352ed60c420f005178b
Quentin Duverge <quentin.duverge@essencemediacom.com>
2023-08-13 06:11:59 +0100

    none

--- main.py
+++ main.py
@@ -9,51 +9,59 @@ from flask import request, jsonify
 app = flask.Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-client = secretmanager.SecretManagerServiceClient()
-secret_name = f"projects/82528465111/secrets/firestore_pulls/versions/latest"
+# client = secretmanager.SecretManagerServiceClient()
+# secret_name = f"projects/82528465111/secrets/firestore_pulls/versions/latest"
 
-response = client.access_secret_version(name=secret_name, )
-secret = json.loads(response.payload.data.decode("UTF-8"))
+# response = client.access_secret_version(name=secret_name, )
+# secret = json.loads(response.payload.data.decode("UTF-8"))
 
-# secret = 'secrets/timelime-dev-7f677154d05e.json'
+secret = 'secrets/timelime-dev-7f677154d05e.json'
 
 cred = credentials.Certificate(secret)
 fbapp = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-users = db.collection('users').get()
-users = [user.to_dict()['email'] for user in users]
+user = None
 
 def token_required(route_function):
     def decorated_function(*args, **kwargs):
+        global user
         print(request)
         try:
             token = request.headers.get("Authorization")
             decoded_token = firebase_admin.auth.verify_id_token(token.split(" ")[1])
         except Exception as e:
+            print(e, flush=True)
             return jsonify({"message": 'token error'}), 401
             
         if not token or not decoded_token:
             return jsonify({"message": "Invalid token"}), 401
+
         
-        if decoded_token['email'] not in users:
-            return jsonify({"message": "Invalid user"}), 401
-        
+        user = db.collection("users").where("email", "==", decoded_token['email']).limit(1)
+
+        try:
+            user = user.get()[0].to_dict()   
+        except:
+            return jsonify({"message": "Invalid user"}), 401   
+
         return route_function(*args, **kwargs)
     
     return decorated_function
 
-@app.route("/")
+@app.route("/", endpoint="index")
 @token_required
 def index():
-    print('COUCOU', flush=True)
+    print(user, flush=True)
     return "Hello World!"
 
-@app.route("/quentin")
+@app.route("/quentin", endpoint="get_quentin")
+@token_required
 def get_quentin():
     return "Hello Quentin!"
 
-@app.route("/data/<collection_name>")
+@app.route("/data/<collection_name>", endpoint="get_timelines")
+@token_required
 def get_timelines(collection_name):
     docs = db.collection(collection_name).get()
     json_data = [doc.to_dict() for doc in docs]
