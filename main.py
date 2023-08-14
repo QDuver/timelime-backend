7af7d747e37b2ec3afead77c5d3d74bacd0de018
Quentin Duverge <quentin.duverge@essenceglobal.com>
2023-08-14 10:23:24 +0100

    none

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ user = None
 def token_required(route_function):
     def decorated_function(*args, **kwargs):
         global user
-        print(request.headers.get("Authorization"))
+        print(request.headers)
         try:
             token = request.headers.get("Authorization")
             decoded_token = firebase_admin.auth.verify_id_token(token.split(" ")[1])
