630c3015caa258f7d1cee88354cb2eaf00c1cc4c
Quentin Duverge <quentin.duverge@essencemediacom.com>
2023-08-31 08:20:26 +0100

    n

--- firestore/firestore_init.py
+++ firestore/firestore_init.py
@@ -6,13 +6,13 @@ import os
 
 def init():
 
-    try:
-        client = secretmanager.SecretManagerServiceClient()
-        secret_name = f"projects/{os.environ.get('GCP_PROJECT_NUMBER')}/secrets/GCP_CREDENTIALS/versions/latest"
-        response = client.access_secret_version(name=secret_name, )
-        secret = json.loads(response.payload.data.decode("UTF-8"))
-    except:
-        secret = 'secrets/timelime-dev-sa.json'
+    # try:
+    client = secretmanager.SecretManagerServiceClient()
+    secret_name = f"projects/{os.environ.get('GCP_PROJECT_NUMBER')}/secrets/GCP_CREDENTIALS/versions/latest"
+    response = client.access_secret_version(name=secret_name, )
+    secret = json.loads(response.payload.data.decode("UTF-8"))
+    # except:
+    #     secret = 'secrets/timelime-dev-sa.json'
 
     cred = credentials.Certificate(secret)
     firebase_admin.initialize_app(cred)
