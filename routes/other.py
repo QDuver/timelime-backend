7576f8a6434e32718aaa88d30960ebaccfdf766d
Quentin Duverge <quentin.duverge@essencemediacom.com>
2023-09-11 20:11:14 +0100

    N

--- routes/other.py
+++ routes/other.py
@@ -5,6 +5,8 @@ from google.cloud import error_reporting
 from utils.methods import get_google_images
 from ai import generate_quiz, process_quiz
 import utils.utils as utils
+import logging
+logger = logging.getLogger('my_logger')
 
 other_bp = Blueprint('other', __name__)
 
@@ -76,10 +78,12 @@ def create_quiz():
 @token_required
 @generic_error_handler
 def get_quizzes(tid):
+    logging.debug('coucou')
     db = app.config['db']
     quizzes = db.get("quizzes", where=('tid', '==', tid), order_by=('created_on', 'ASCENDING'))
     for quiz in quizzes:
         quiz = process(quiz)
+    
     return jsonify({"quizzes": quizzes}), 200
 
 @other_bp.route("/submit-quiz/", endpoint="submit_quiz", methods=['POST'])
