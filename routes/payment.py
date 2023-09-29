f6169ebabdcdfbda46ed3c4badf26a4b11617b62
Quentin Duverge <quentin.duverge@essencemediacom.com>
2023-09-29 12:58:30 +0100

    n

--- routes/payment.py
+++ routes/payment.py
@@ -80,6 +80,9 @@ def create_subscription(session_id, uid):
     setup_intent = stripe.SetupIntent.retrieve( session["setup_intent"] )
     payment_method = setup_intent["payment_method"]
     print('CREATING CUSTOMER', flush=True)
-    customer = stripe.Customer.create(description=uid, email=user['email'], metadata={'uid': uid}, payment_method=payment_method )
+    customer = stripe.Customer.create(description=uid, email=user['email'], metadata={'uid': uid} )
+    stripe.PaymentMethod.attach(payment_method, customer=customer.id)
+    customer = stripe.Customer.retrieve(customer.id)
+    print('CUSTOMER', customer, flush=True)
     # stripe.Subscription.create( customer=customer.id, items=[ {"price": prices.data[0].id}, ], trial_period_days=1, metadata={'uid': uid}, )
     stripe.Subscription.create( customer=customer.id, items=[ {"price": prices.data[0].id}, ],  metadata={'uid': uid}, )
